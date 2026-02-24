24965c5824e3dd49614c224b5982f0f1c0c55676
dotcrph <86572143+dotcrph@users.noreply.github.com>
2026-02-23 20:08:57 -0500

    Logging: add lastLogMessage

--- src/te_logging.py
+++ src/te_logging.py
@@ -8,6 +8,8 @@ os.makedirs(logsDir, exist_ok=True)
 logFilename: str = datetime.now().strftime("log_%y-%m-%d_%H-%M-%S.txt")
 logPath: str = os.path.join(logsDir, logFilename)
 
+lastLogMessage = ""
+
 def error(msg: str):
     prefix("ERROR", msg)
 
@@ -15,7 +17,9 @@ def info(msg: str):
     prefix("Info", msg)
 
 def prefix(prefix: str, msg: str):
-    time: str = datetime.now().strftime("%H:%M:%S")
+    global lastLogMessage
+    lastLogMessage = f"[{prefix}] {msg}"
 
+    time: str = datetime.now().strftime("%H:%M:%S")
     with open(logPath, "a", encoding="utf-8") as f:
-        f.write(f"({time}) [{prefix}] {msg}\n")
+        f.write(f"({time}) " + lastLogMessage + "\n")

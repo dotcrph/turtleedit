27f42b7c67850b7d7add4f95ca4a713a1e616d8f
dotcrph <86572143+dotcrph@users.noreply.github.com>
2026-02-23 19:19:03 -0500

    Logging: add info()

--- src/te_logging.py
+++ src/te_logging.py
@@ -11,8 +11,11 @@ logPath: str = os.path.join(logsDir, logFilename)
 def error(msg: str):
     prefix("ERROR", msg)
 
+def info(msg: str):
+    prefix("Info", msg)
+
 def prefix(prefix: str, msg: str):
     time: str = datetime.now().strftime("%H-%M-%S")
 
     with open(logPath, "a", encoding="utf-8") as f:
-        f.write(f"({time}) [{prefix}] {msg}")
+        f.write(f"({time}) [{prefix}] {msg}\n")

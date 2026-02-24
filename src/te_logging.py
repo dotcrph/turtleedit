fdd6061a095d7100f7cedfc8016c84a463d9eabc
dotcrph <86572143+dotcrph@users.noreply.github.com>
2026-02-23 19:21:54 -0500

    Logging: change time formatting

--- src/te_logging.py
+++ src/te_logging.py
@@ -15,7 +15,7 @@ def info(msg: str):
     prefix("Info", msg)
 
 def prefix(prefix: str, msg: str):
-    time: str = datetime.now().strftime("%H-%M-%S")
+    time: str = datetime.now().strftime("%H:%M:%S")
 
     with open(logPath, "a", encoding="utf-8") as f:
         f.write(f"({time}) [{prefix}] {msg}\n")

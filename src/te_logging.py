02238765920cdcf2640cb85b7dd10990d8377d63
dotcrph <86572143+dotcrph@users.noreply.github.com>
2026-03-04 05:48:05 -0500

    Logging: call addToAPI

--- src/te_logging.py
+++ src/te_logging.py
@@ -1,23 +1,33 @@
+import te_api as api
+
 from datetime import datetime
 import os
 import os.path
 
 logsDir: str = "logs"
 os.makedirs(logsDir, exist_ok=True)
+api.addToAPI("log", logsDir)
 
 logFilename: str = datetime.now().strftime("log_%y-%m-%d_%H-%M-%S.txt")
+api.addToAPI("log", logFilename)
+
 logPath: str = os.path.join(logsDir, logFilename)
+api.addToAPI("log", logPath)
 
 lastLogMessage = ""
+api.addToAPI("log", lastLogMessage)
 
 def error(msg: str):
     prefix("ERROR", msg)
+api.addToAPI("log", error)
 
 def warn(msg: str):
     prefix("WARNING", msg)
+api.addToAPI("log", warn)
 
 def info(msg: str):
     prefix("Info", msg)
+api.addToAPI("log", info)
 
 def prefix(prefix: str, msg: str):
     global lastLogMessage
@@ -30,3 +40,4 @@ def prefix(prefix: str, msg: str):
     print(logMsgWithTime)
     with open(logPath, "a", encoding="utf-8") as f:
         f.write(logMsgWithTime + "\n")
+api.addToAPI("log", prefix)

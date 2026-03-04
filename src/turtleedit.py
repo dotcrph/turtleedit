1f9e065a3c88c89a9cee85b04cbde5c416d7e738
dotcrph <86572143+dotcrph@users.noreply.github.com>
2026-03-04 18:27:52 -0500

    Turtleedit: add binds in main()

--- src/turtleedit.py
+++ src/turtleedit.py
@@ -1,6 +1,7 @@
 import te_cfg as cfg
 import te_widgets as widgets
 import te_plugins as plugins
+import te_binds as binds
 
 from os.path import join
 
@@ -10,11 +11,13 @@ def main() -> None:
     widgets.placeWidgets()
     widgets.initializeWidgets()
 
+    binds.initializeEventHandlers()
+    binds.initializeBinds()
+
     plugins.loadPlugins()
 
     widgets.frame.Show()
     widgets.app.MainLoop()
-
 
 if __name__ == '__main__':
     main()

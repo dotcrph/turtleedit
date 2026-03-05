26de602892b199b2d5dfc6d08e0f563b687d150b
dotcrph <86572143+dotcrph@users.noreply.github.com>
2026-03-04 19:52:49 -0500

    Plugins: fix local imports not working

--- src/te_plugins.py
+++ src/te_plugins.py
@@ -1,11 +1,14 @@
 import te_api as api
 import te_logging as log
-import te_io as io
+import te_info as info
 
+from sys import path
+from os import getcwd, chdir
 from os.path import isfile, join
 
-initFilename = "init.py"
-initDir = join("plugins", initFilename)
+pluginsDir      = "plugins"
+initFilename    = "init.py"
+initDir         = join(pluginsDir, initFilename)
 
 def loadPlugins() -> None:
     if not isfile(initDir):
@@ -16,16 +19,30 @@ def loadPlugins() -> None:
         with open(initDir, 'r', encoding='utf-8') as initFile:
             initFileAsString = initFile.read()
     except Exception as e:
-        if isinstance(e, tuple(io.ioErrors.keys())):
-            log.error(io.ioErrors[type(e)].format(initDir) 
+        if isinstance(e, tuple(info.ioErrors.keys())):
+            log.error(info.ioErrors[type(e)].format(initDir) 
                      + f"({e})")
             return
         else:
             raise
 
     log.info("Loading plugins...")
-
     initCompiled = compile(initFileAsString, initFilename, 'exec')
-    exec(initCompiled, api.apiGlobals)
+
+    originalDir = getcwd()
+    pluginsFullDir = join(originalDir, pluginsDir)
+
+    log.info(f"Executing '{initFilename}' from '{pluginsFullDir}'")
+    chdir(pluginsFullDir)
     
+    if (pluginsFullDir not in path):
+        path.append(pluginsFullDir)
+
+    exec(initCompiled, api.apiGlobals)
+
+    path.remove(pluginsFullDir)
+
+    chdir(originalDir)
+    log.info(f"Changed working directory back to '{originalDir}'")
+
     log.info("Successfully loaded plugins!")

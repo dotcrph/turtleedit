5ad9284039326807189d2e76745b58a957c565f1
dotcrph <86572143+dotcrph@users.noreply.github.com>
2026-03-04 19:54:13 -0500

    Create examples/minimal

---
+++ examples/minimal/plugins/init.py
@@ -0,0 +1,5 @@
+# This script is meant to initialize other plugins, load configs, setup binds, 
+# etc
+
+# You can uncomment the line below to see what happens
+# te.widgets.setTitle("Hello from init.py!")

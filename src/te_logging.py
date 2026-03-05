64b6f49bdc9c29dadece10f6cdd124085d36fd7d
dotcrph <86572143+dotcrph@users.noreply.github.com>
2026-03-05 18:58:00 -0500

    Logging: wrap vars in Mutable

--- src/te_logging.py
+++ src/te_logging.py
@@ -4,18 +4,20 @@ from datetime import datetime
 import os
 import os.path
 
-logsDir: str = "logs"
-os.makedirs(logsDir, exist_ok=True)
+logsDir: api.Mutable[str] = api.Mutable("logs")
 api.addToAPI("log", logsDir)
 
-logFilename: str = datetime.now().strftime("log_%y-%m-%d_%H-%M-%S.txt")
+logFilename: api.Mutable[str]                                                 \
+    = api.Mutable(datetime.now().strftime("log_%y-%m-%d_%H-%M-%S.txt"))
 api.addToAPI("log", logFilename)
 
-logPath: str = os.path.join(logsDir, logFilename)
+logPath: api.Mutable[str] = api.Mutable(os.path.join(logsDir.v, logFilename.v))
 api.addToAPI("log", logPath)
 
-lastLogMessage = ""
+lastLogMessage: api.Mutable[str] = api.Mutable("")
 api.addToAPI("log", lastLogMessage)
+
+os.makedirs(logsDir.v, exist_ok=True)
 
 def error(msg: str):
     prefix("ERROR", msg)
@@ -31,13 +33,13 @@ api.addToAPI("log", info)
 
 def prefix(prefix: str, msg: str):
     global lastLogMessage
-    lastLogMessage = f"[{prefix}] {msg}"
+    lastLogMessage.v = f"[{prefix}] {msg}"
 
     time: str = datetime.now().strftime("%H:%M:%S")
 
-    logMsgWithTime: str = f"({time}) {lastLogMessage}"
+    logMsgWithTime: str = f"({time}) {lastLogMessage.v}"
 
     print(logMsgWithTime)
-    with open(logPath, "a", encoding="utf-8") as f:
+    with open(logPath.v, "a", encoding="utf-8") as f:
         f.write(logMsgWithTime + "\n")
 api.addToAPI("log", prefix)
